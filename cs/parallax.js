83baa9fba285e43c4ebac04236ce0ed7db57d901
Michael Jennings <mpjngs@gmail.com>
2015-05-14 13:40:50 -0700

    parallax working, without offset though

--- cs/parallax.js
+++ cs/parallax.js
@@ -5,9 +5,14 @@ function parallaxInit(){
   function setupParallax(img, container){
     var bounds
 
+
+    function getScroll(){
+      return window.pageYOffset
+    }
+
     function getBounds(){
       var bb = container.getBoundingClientRect()
-      var scrollTop = document.body.scrollTop
+      var scrollTop = getScroll()
       bounds = [scrollTop + bb.top, scrollTop + bb.bottom]
     }
     getBounds()
@@ -17,19 +22,28 @@ function parallaxInit(){
     }
   
     function parallax(){
-      var position = document.body.scrollTop
+      var position = getScroll() 
     
       if(position < bounds[1] && position > bounds[0] - viewportHeight){
-        console.log(bounds, position)
         img.style.transform = transform((position + (viewportHeight / 2) - ((bounds[0] + bounds[1]) / 2)) * ratio)
       }
     }
   
+    function resizeListener(func){
+      var after
+      function onResize(){
+        window.clearTimeout(after)
+        after = window.setTimeout(func, 100)
+        func()
+      }
+      window.addEventListener('resize', onResize)
+    }
+
     window.addEventListener('scroll', parallax)
     window.addEventListener('bodyChange', getBounds)
     window.addEventListener('bodyChange', parallax)
-    window.addEventListener('resize', getBounds)
-    window.addEventListener('resize', parallax)
+    resizeListener(getBounds)
+    resizeListener(parallax)
   }
   
   for(var i = 0; i < pbr.parallax.bulks.length; i++){
