1a85d5927ce50e67a5450b43e42e0e3a8fada251
Michael Jennings <mpjngs@gmail.com>
2015-05-14 18:48:08 -0700

    comments

--- cs/sections.js
+++ cs/sections.js
@@ -11,21 +11,32 @@ function pageMover(position){
   window.scrollTo(0, position)
 }
 
+//this variable is for the sake of other code
+//it must be true when we are scrolling automatically
+//to a section and false otherwise
+//once we are done scrolling, we must fire a 
+//'doneTravelling' event
 var travelling = false
 
 function toSection(section){
-
+ 
+  //where we are scrolling from
   var start = document.body.scrollTop
+  //where we are going to scroll to
   var target
 
+  //if a section has been specified, go to that section,
+  //otherwise go to the top of the page
   if(section !== undefined){
+ 
+    //get the height of the document and viewport
     var body = document.body,
         html = document.documentElement;
-  
     var documentHeight = Math.max( body.scrollHeight, body.offsetHeight, 
                            html.clientHeight, html.scrollHeight, html.offsetHeight );
     var viewportHeight = window.innerHeight 
   
+    //get the midpoint of the section we are going to
     var bounds = sections[section].getBoundingClientRect()
     var mid = (bounds.top + bounds.bottom) / 2
     
@@ -42,6 +53,7 @@ function toSection(section){
     window.dispatchEvent(new Event('doneTravelling'))
   }
 
+  //scroll
   new MoveGen(pageMover, 1)
   .ends(start, target)
   .acceleration(1, 1, .4)
--- cs/sun.js
+++ cs/sun.js
@@ -17,59 +17,81 @@ function lazyListener(target, evt, func, timeGap){
   }
 }
 
+/*
+  Sets up the suns.
+
+  The suns should have the following behavior:
+
+  When a sun first enters the viewport, it should spin
+  and fade in for a certain amount of time, unless we are
+  currently scrolling to a section automatically, due to a
+  user having clicked a menu item. If are automatically
+  scrolling, we should wait until it is done travelling,
+  at which point a 'doneTravelling' event will be fired,
+  and then spin in whatever suns are in the viewport.
+*/
+
 function setupSuns(){
 
-  function spinner(sun){
-    return function(pos){
-      sun.style.transform = 'rotate(' + pos + "turn)"
-    }
-  }
-  function fader(sun){
-    return function(pos){
-      sun.style.opacity = pos
-    }
-  }
+  //when called, makes a sun spin and fade in
   function spinIn(sun){
+
+    function spinner(pos){ sun.style.transform = 'rotate(' + pos + "turn)" }
+    function fader(pos){ sun.style.opacity = pos }
+
     var time = .8
-    new MoveGen(spinner(sun), time)
+    new MoveGen(spinner, time)
     .ends(0, .7)
     .acceleration(0, 1)
     .run()
-    new MoveGen(fader(sun), time)
+    new MoveGen(fader, time)
     .ends(0, 1)
     .acceleration(0, 1)
     .run()
   }
 
-  function getPosition(element){
-    var bb = element.getBoundingClientRect()
-    var scrollTop = window.pageYOffset 
-    return scrollTop + (bb.top + bb.bottom) / 2
-  }
-
+  //sets up a sun to spin in when it is within the viewport
   function setupSun(sun){
+    //the vertical position of the sun
     var pos
 
+    //update pos
     function updatePosition(){
-      pos = getPosition(sun)
+      var bb = sun.getBoundingClientRect()
+      var scrollTop = window.pageYOffset 
+      pos = scrollTop + (bb.top + bb.bottom) / 2
     }
 
+    //an array of functions that will remove all the sun's
+    //event listeners when called
     var stops = []
     
     function onScroll(){
       var scroll = window.pageYOffset
 
+      /*travelling is a variable from sections.js. When it is true, the page
+      is scrolling automatically to a section, due to a menu item having been
+      clicked by the user. When travelling is true, we want to wait until the 
+      page has stopped moving before we spin in any suns. When the page has 
+      finished scrolling, a 'doneTravelling' event will be fired*/
+
+      //if not travelling and the sun is within the viewport
       if(!travelling && pos > scroll && pos < scroll + viewportHeight){
         spinIn(sun)
+        //remove all the sun's event listeners
         for(var i = 0; i < stops.length; i++){
           stops[i]()
         } 
       }
     }
 
+    //keep track of the position of the sun
     stops.push(lazyListener(window, 'resize', updatePosition, 200))
     stops.push(lazyListener(window, 'bodyChange', updatePosition, 200))
 
+    //check if the sun should spin in
+    //'doneTravelling' is fired after we have just finished automatically 
+    //scrolling to a section
     window.addEventListener('scroll', onScroll)
     window.addEventListener('doneTravelling', onScroll)
     stops.push(function(){
@@ -79,6 +101,7 @@ function setupSuns(){
       window.removeEventListener('doneTravelling', onScroll)
     }) 
 
+    //get the initial position
     updatePosition()
     window.addEventListener('load', updatePosition)
   }
